7cb2bb3c2580f4cb142caac6b53f4f7c3b691c79
Colin Stevenson <colinstevenson@Colins-MacBook-Pro.local>
2018-02-01 21:22:36 -0700

    Adds rudimentary attendance UI

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -4,8 +4,14 @@
     <h1>{{Event.name}}</h1>
     <section class="card">
       <div class="card-body">
-        {{Event.time}}
+        {{$moment(Event.time).format('MMM Do [at] hh:mma')}}
       </div>
+      <div class="card-body">
+        <div class="h3">Are you in?</div>
+        <button class="btn btn-success disabled">Yes</button>
+        <button class="btn btn-danger">No</button>
+        <button class="btn btn-link">Maybe</button>
+      </div>  
     </section>
   </div>
 </template>
