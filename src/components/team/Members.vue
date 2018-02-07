7c0a5b209d51ded7ec1429b45eae585bb17c9028
Colin Stevenson <colin.stevenson@gmail.com>
2018-02-06 20:35:02 -0700

    Adds email validation to invite form

--- src/components/team/Members.vue
+++ src/components/team/Members.vue
@@ -15,12 +15,16 @@
       </template>
     </ul>
     <div class="card-body" v-if="isAdmin">
-      <div class="form-inline" v-if="isInviting">
+      <div class="form" v-if="isInviting">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Email address" v-model="email">
+          <label for="invite-email-input">Invite new member</label>
+          <input type="text" class="form-control" :class="isValidEmail ? '' : 'is-invalid'" placeholder="Email address" id="invite-email-input" v-model="email">
+          <small v-if="!isValidEmail" class="invalid-feedback">Invitation is pending or the email is not valid.</small>
         </div>
-        <button class="btn btn-primary mx-sm-3" type="button" @click="sendInvitation">Send</button>
-        <button class="btn btn-danger" type="button" @click="toggleInviting">Cancel</button>
+        <div class="form-group">
+          <button class="btn btn-primary mr-sm-1" type="button" @click="sendInvitation">Send</button>
+          <button class="btn btn-danger" type="button" @click="toggleInviting">Cancel</button>
+        </div>
       </div>
       <div v-else>
         <button class="btn btn-primary card-link" @click="toggleInviting">Invite people</button>
@@ -39,7 +43,8 @@ export default {
       teamMembersLoading: 'teamMembersLoading',
       isAdmin: 'isAdmin',
       roleLoading: 'roleLoading',
-      unacceptedInvitations: 'unacceptedInvitations'
+      unacceptedInvitations: 'unacceptedInvitations',
+      user: 'user'
     }),
     isLoading () {
       return this.roleLoading || this.teamMembersLoading
@@ -48,25 +53,42 @@ export default {
   data () {
     return {
       isInviting: false,
-      email: ''
+      email: '',
+      isValidEmail: true
     }
   },
   methods: {
     toggleInviting () {
       this.isInviting = !this.isInviting
+      this.isValidEmail = true
     },
     sendInvitation () {
-      this.$store.dispatch('sendInvitation', {
-        email: this.email,
-        teamId: this.teamId,
-        userId: 'cjbs2ctasnn7b01702y18svyo'
-      })
-      this.email = ''
+      if (this.validateInvitation()) {
+        this.$store.dispatch('sendInvitation', {
+          email: this.email,
+          teamId: this.teamId,
+          userId: this.user.id
+        })
+        this.email = ''
+      } else {
+        // handle error
+        console.error('invitation already exists')
+      }
     },
     requestUnacceptedInvitations () {
       this.$store.dispatch('getUnacceptedInvitations', {
         teamId: this.teamId
       })
+    },
+    validateInvitation () {
+      let isValid = !(/^\s*$/.test(this.email))
+      if (isValid) {
+        isValid = !this.unacceptedInvitations.find(invite => {
+          return invite.email.toLowerCase() === this.email.toLowerCase()
+        })
+      }
+      this.isValidEmail = isValid
+      return isValid
     }
   },
   mounted () {
@@ -81,6 +103,9 @@ export default {
       if (this.isAdmin) {
         this.requestUnacceptedInvitations()
       }
+    },
+    email () {
+      this.isValidEmail = true
     }
   }
 }
