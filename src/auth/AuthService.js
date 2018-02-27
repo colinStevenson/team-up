dd6f5ba0228ce92b7a9d6b4a8934c4c6f142d02f
Colin Stevenson <colin.stevenson@gmail.com>
2018-02-26 21:16:05 -0700

    Updates logout redirect

--- src/auth/AuthService.js
+++ src/auth/AuthService.js
@@ -60,7 +60,7 @@ class AuthService {
     this.userProfile = null
     this.authNotifier.emit('authChange', false)
     // navigate to the home route
-    router.replace('home')
+    router.replace({name: 'Home'})
   }
 
   isAuthenticated () {
